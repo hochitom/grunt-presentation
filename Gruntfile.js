83f71562080c4f81f43e4b71cfd92589abd072f5
hochitom <hochitom@icloud.com>
2013-10-15 07:32:35 +0200

    added uglify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,18 @@
 module.exports = function (grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.initConfig({
-        
+        uglify: {
+            options: {
+                mangle: false
+            },
+            default: {
+                src: ['app/scripts/main.js'],
+                dest: 'app/scripts/main.min.js'
+            }
+        }
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['uglify']);
 };
