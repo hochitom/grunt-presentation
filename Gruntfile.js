5aa33fd2e194c5e028c617260d3e6723e6b5ea25
Thomas Hochoertler <t.hochoertler@diamonddogs.cc>
2013-10-16 16:00:32 +0200

    removed files, added tasks to default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,15 @@ var mountFolder = function (connect, dir) {
 
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-spritesmith');
     grunt.loadNpmTasks('grunt-modernizr');
+    grunt.loadNpmTasks('grunt-open');
 
     grunt.initConfig({
         uglify: {
@@ -18,14 +20,7 @@ module.exports = function (grunt) {
                 mangle: false
             },
             default: {
-                options: {
-                    beautify: true
-                },
-                src: ['app/scripts/main.js', 'app/scripts/scripts.js'],
-                dest: 'app/scripts/main.ck.js'
-            },
-            build: {
-                src: ['app/scripts/main.js', 'app/scripts/scripts.js'],
+                src: ['app/scripts/main.js'],
                 dest: 'app/scripts/main.min.js'
             }
         },
@@ -33,19 +28,33 @@ module.exports = function (grunt) {
             options: {
                 style: 'expanded',
                 debugInfo: true,
-                lineNumbers: true
+                lineNumbers: true,
+                sourcemap: true
             },
-            files: [{
-                    'app/styles/main.css': 'scss/*.scss'
-                }]
+            default: {
+                files: [{'app/styles_dev/main.css': 'scss/**/*.scss'}]
+            },
+            build: {
+                options: {
+                    style: 'compressed',
+                    debugInfo: false,
+                    lineNumbers: false,
+                    sourcemap: false
+                },
+                files: [{'app/styles/main.min.css': 'scss/**/*.scss'}]
+            }
         },
         watch: {
             scripts: {
-                files: ['app/scripts/**/*.js', '!app/scripts/**/*.min.js'],
-                tasks: ['uglify:build']
+                files: ['app/styles/*.js'],
+                tasks: ['uglify']
+            },
+            css: {
+                files: ['scss/*.scss'],
+                tasks: ['sass:default']
             },
             livereload: {
-                files: ['app/styles/*.css'],
+                files: ['app/styles_dev/*.css'],
                 options: {
                     livereload: true
                 }
@@ -53,7 +62,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 3000,
+                port: 9000,
                 hostname: 'localhost'
             },
             default: {
@@ -61,17 +70,18 @@ module.exports = function (grunt) {
                     middleware: function (connect) {
                         return [
                             require('connect-livereload')({
-                                src: 'http://localhost:35729/livereload.js?snipver=1'
+                                src: "http://localhost:35729/livereload.js?snipver=1"
                             }),
                             mountFolder(connect, 'app')
-                        ]
+                        ];
                     }
                 }
             }
         },
         open: {
             default: {
-                path: 'http://localhost:3000'
+                path: 'http://localhost:9000',
+
             }
         },
         imagemin: {
@@ -79,7 +89,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'app/images',
-                    src: '**/*.{png,jpg,jpeg}',
+                    src: '{,*/}*.{png,jpg,jpeg}',
                     dest: 'app/images'
                 }]
             }
@@ -97,6 +107,6 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['uglify:default', 'connect', 'open', 'watch']);
-    grunt.registerTask('build', ['uglify:build', 'imagemin', 'modernizr']);
+    grunt.registerTask('default', ['uglify:default', 'sass:default', 'connect:default', 'open', 'watch']);
+    grunt.registerTask('build', ['uglify:default', 'sass:build', 'imagemin:build', 'modernizr']);
 };
